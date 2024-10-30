5fa05f8c2a5ba15802d4fccccbc497b324a68629
Leonardo Jaques <leonardo.jacques@amazonasenergia.com>
2024-10-30 15:06:26 -0400

    reactor: removed sleep method and removed by=By.ID because it is the default value

--- script.py
+++ script.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import pandas as pd
-from time import sleep
  
 sevirce = Service(ChromeDriverManager().install())
 options = webdriver.ChromeOptions() 
@@ -14,23 +13,23 @@ driver = webdriver.Chrome(service=sevirce, options=options)
 
 driver.get("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 
-driver.implicitly_wait(2)  # Ajustei para 2 segundos para garantir tempo de carregamento
+driver.implicitly_wait(2)  
 
-email = driver.find_element(by=By.ID, value="email").send_keys("pythonimpressionador@gmail.com")
-senha = driver.find_element(by=By.ID, value="password").send_keys('sua senha')  # Verifique o ID correto do campo de senha
-submit = driver.find_element(by=By.ID, value='pgtpy-botao').click()
+email = driver.find_element(value="email").send_keys("pythonimpressionador@gmail.com")
+senha = driver.find_element(value="password").send_keys('sua_senha') 
+submit = driver.find_element(value='pgtpy-botao').click()
 
 df = pd.read_csv('produtos.csv')
 
 for line in df.iterrows():
     
-    codigo = driver.find_element(by=By.ID, value="codigo").send_keys(line[1]['codigo'])
-    marca = driver.find_element(by=By.ID, value="marca").send_keys(line[1]['marca'])
-    tipo = driver.find_element(by=By.ID, value="tipo").send_keys(line[1]['tipo'])
-    categoria = driver.find_element(by=By.ID, value="categoria").send_keys(line[1]['categoria'])
-    preco_unitario = driver.find_element(by=By.ID, value="preco_unitario").send_keys(line[1]['preco_unitario'])
-    custo = driver.find_element(by=By.ID, value="custo").send_keys(line[1]['custo'])
+    codigo = driver.find_element(value="codigo").send_keys(line[1]['codigo'])
+    marca = driver.find_element(value="marca").send_keys(line[1]['marca'])
+    tipo = driver.find_element(value="tipo").send_keys(line[1]['tipo'])
+    categoria = driver.find_element(value="categoria").send_keys(line[1]['categoria'])
+    preco_unitario = driver.find_element(value="preco_unitario").send_keys(line[1]['preco_unitario'])
+    custo = driver.find_element(value="custo").send_keys(line[1]['custo'])
     if not pd.isna(line[1]['obs']):
-        obs = driver.find_element(by=By.ID, value="obs").send_keys(line[1]['obs'])
-    submit = driver.find_element(by=By.ID, value='pgtpy-botao').click()
+        obs = driver.find_element(value="obs").send_keys(line[1]['obs'])
+    submit = driver.find_element(value='pgtpy-botao').click()
     
